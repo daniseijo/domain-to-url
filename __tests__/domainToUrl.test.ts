16a481dc80cf3c7d5075392288bbe3fbf4f4ede3
Daniel Seijo Sánchez <daniseijo12@gmail.com>
2019-03-08 23:41:20 +0100

    Add test coverage to get to 100%

--- __tests__/domainToUrl.test.ts
+++ __tests__/domainToUrl.test.ts
@@ -4,4 +4,14 @@ describe('domainToUrl', () => {
   it('should convert scheme and domain to url', () => {
     expect(domainToUrl('https', 'test.com')).toBe('https://www.test.com')
   })
+  it('should fail when scheme is not http or https', () => {
+    expect(() => {
+      domainToUrl('fail', 'test.com')
+    }).toThrowError()
+  })
+  it('should fail when domain has more than 2 dots', () => {
+    expect(() => {
+      domainToUrl('http', 'api.test.com')
+    }).toThrowError()
+  })
 })
